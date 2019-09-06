64ea3a716d1ef942a31cac476e6ddaff952aa9d7
schieljn <schieljn@fius.informatik.uni-stuttgart.de>
2019-09-06 16:20:03 +0200

    Added nom

---
+++ nomnom.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3.7
+
+import random
+nom=[
+"Schnitzel",
+"Panierter Blumenkohl",
+"Kässpätzle",
+"Maultaschen in Brühe",
+"Maultaschen mit Ei",
+"Eierspätzle",
+"Spaghetti",
+"Pizza",
+"Döner",
+"Maultaschen mit Spätzle",
+"Kartoffelgratin",
+"Wurstsalat",
+"Pilzsuppe",
+"Spätzepfanne",
+"Spargel",
+"Spätzleauflauf",
+"Brezelauflauf",
+"Kürbissuppe",
+"Flammkuchen",
+"Burrito",
+"Zwiebelrostbraten",
+"Hawai Toast".
+"Arme Ritter",
+"Gefüllte Paprika",
+"Pfannkuchen",
+"Milchreis",
+"Crêpes",
+"Schinkennudeln",
+"Flädlesuppe",
+"Nudelsuppe mit Ei",
+"Falafel",
+"Reispfanne",
+"Chili con Carne",
+]
+
+print(nom[random.randrange(0,len(nom))])
